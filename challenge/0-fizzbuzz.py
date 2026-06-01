5e1ca9310e6736af5c34c0db3f0895086702ba79
Rionhaato <110690749+Rionhaato@users.noreply.github.com>
2026-06-01 19:32:14 -0400

    Keep task 0 fix scoped to starter code

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
+""" FizzBuzz
 """
-FizzBuzz - Print the numbers from 1 to n separated by a space.
-
-For multiples of three print "Fizz" instead of the number and for
-multiples of five print "Buzz". For numbers which are multiples of both
-three and five print "FizzBuzz".
-"""
+import sys
 
 
 def fizzbuzz(n):
-    """Return the FizzBuzz sequence from 1 to n."""
-    tmp_result = []
+    """
+    FizzBuzz function prints numbers from 1 to n separated by a space.
 
+    - For multiples of three print "Fizz" instead of the number and for
+      multiples of five print "Buzz".
+    - For numbers which are multiples of both three and five print "FizzBuzz".
+    """
+    if n < 1:
+        return
+
+    tmp_result = []
     for i in range(1, n + 1):
         if (i % 3) == 0 and (i % 5) == 0:
             tmp_result.append("FizzBuzz")
@@ -21,21 +25,15 @@ def fizzbuzz(n):
             tmp_result.append("Buzz")
         else:
             tmp_result.append(str(i))
-
-    return tmp_result
+    print(" ".join(tmp_result))
 
 
 if __name__ == '__main__':
-    import sys
-
-    if len(sys.argv) < 2:
-        print("Usage: {} <number>".format(sys.argv[0]))
+    if len(sys.argv) <= 1:
+        print("Missing number")
+        print("Usage: ./0-fizzbuzz.py <number>")
+        print("Example: ./0-fizzbuzz.py 89")
         sys.exit(1)
 
-    try:
-        number = int(sys.argv[1])
-    except ValueError:
-        print("Usage: {} <number>".format(sys.argv[0]))
-        sys.exit(1)
-
-    print(" ".join(fizzbuzz(number)))
+    number = int(sys.argv[1])
+    fizzbuzz(number)
